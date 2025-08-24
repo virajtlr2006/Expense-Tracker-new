0c60e9dce63abb052ebed4509e289964f08c723a
Viraj Tailor <virajtlr2006@gmail.com>
2025-08-24 15:48:07 +0530

    Dashboard 90% done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from './Components/NavBar'
 import Dashboard from './Components/Dashboard'
 import Workflow from './Components/Workflow'
+import FewEntry from './Components/FewEntry'
 
 const App = () => {
   return (
-    <div className='bg-gray-800 h-screen'>
+    <div className='bg-gray-800 '>
       <NavBar/>
       <Dashboard/>
       <Workflow/>
+      <FewEntry/>
     </div>
   )
 }
---
+++ frontend/src/Components/FewEntry.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const FewEntry = () => {
+  return (
+    <div className='mt-20 pl-65 h-screen'>
+      <p className='text-white text-2xl mb-10'>
+        To : Viraj Tailor &nbsp;&nbsp;&nbsp;&nbsp;
+        Type : Credit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Expense : Food&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Amount : 500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <p className='text-white text-2xl mb-10'>
+        To : Viraj Tailor &nbsp;&nbsp;&nbsp;&nbsp;
+        Type : Credit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Expense : Food&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Amount : 500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <p className='text-white text-2xl mb-10'>
+        To : Viraj Tailor &nbsp;&nbsp;&nbsp;&nbsp;
+        Type : Credit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Expense : Food&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Amount : 500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <p className='text-white text-2xl mb-10'>
+        To : Viraj Tailor &nbsp;&nbsp;&nbsp;&nbsp;
+        Type : Credit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Expense : Food&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Amount : 500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+      <p className='text-white text-2xl '>
+        To : Viraj Tailor &nbsp;&nbsp;&nbsp;&nbsp;
+        Type : Credit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Expense : Food&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Amount : 500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </p>
+
+    <div className='mt-10  mx-75 '>
+      <button className='text-white border-4 border-emerald-600 bg-emerald-600 rounded-2xl px-5'>
+        View More
+      </button>
+    </div>
+    </div>
+  )
+}
+
+export default FewEntry
