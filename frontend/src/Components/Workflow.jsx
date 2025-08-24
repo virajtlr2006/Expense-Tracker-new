d47756fb786c59368e1bbcddc32ed3528d8b97c2
Viraj Tailor <virajtlr2006@gmail.com>
2025-08-24 15:22:57 +0530

    Done

--- frontend/src/Components/Workflow.jsx
+++ frontend/src/Components/Workflow.jsx
@@ -4,15 +4,15 @@ const Workflow = () => {
   return (
     <div className='  mt-15   '>
 
-      <div className='flex justify-end mr-15'>
+      <div className='flex justify-end mr-15 '>
 
-        <div className=' flex mr-60 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-900 to-emerald-800 px-10'>
-          <p className='h-1 text-5xl text-white text-transparent '>Your Transactions</p>
+        <div className=' flex bg-gradient-to-r from-blue-800 to bg-emerald-800 mr-60 rounded-2xl overflow-hidden  px-10 '>
+          <p className='h-1 text-5xl text-white '>Your Transactions</p>
         </div>
 
 
         <div className='flex mr-10 border-4 rounded-2xl border-emerald-600 overflow-hidden '>
-          <input type="search" placeholder='Search your transaction' className='h-12 w-60 text-white placeholder:text-white pl-9' />
+          <input type="search" placeholder='Search your transaction' className='h-10 w-60 text-white placeholder:text-white pl-9' />
         </div>
 
 
