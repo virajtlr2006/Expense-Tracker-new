cc84cd0758bbdb7262af3e3c0d4a11e12af55fb3
Viraj Tailor <virajtlr2006@gmail.com>
2025-08-24 13:42:57 +0530

    Add expense button and search div completed

---
+++ frontend/src/Components/FeatureSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeatureSection = () => {
+  return (
+    <div> </div>
+  )
+}
+
+export default FeatureSection
--- frontend/src/Components/Workflow.jsx
+++ frontend/src/Components/Workflow.jsx
@@ -1,25 +1,30 @@
-import { BadgePlus, CreditCardIcon, File, ListPlus } from 'lucide-react'
 import React from 'react'
 
 const Workflow = () => {
   return (
-    <div className='  mt-20 '>
+    <div className='  mt-15   '>
 
-      {/* Add Expense */}
-      <div className='flex items-center'>
-        <ListPlus className='text-green-800 ml-8 h-14 w-14' />
-        <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ml-15' >Add Expense</button>
-        <p className='text-xl ml-12 text-transparent bg-gradient-to-r from-white to-emerald-600 bg-clip-text'>Easily add your daily expenses and income with just a few clicks. Stay on top of your budget by tracking every transaction in real time.</p>
+      <div className='flex justify-end mr-15'>
+
+        <div className=' flex mr-60 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-900 to-emerald-800 px-10'>
+          <p className='h-1 text-5xl text-white text-transparent '>Your Transactions</p>
+        </div>
+
+
+        <div className='flex mr-10 border-4 rounded-2xl border-emerald-600 overflow-hidden '>
+          <input type="search" placeholder='Search your transaction' className='h-12 w-60 text-white placeholder:text-white pl-9' />
+        </div>
+
+
+        <div>
+          <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ' >Add Expense</button>
+        </div>
+
       </div>
-      {/* Track expense */}
-      <div className='flex items-center mt-8'>
-        <CreditCardIcon className='text-green-800 ml-8 h-14 w-14' />
-        <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ml-17'>Track Expense</button>
-        <p className='text-xl ml-14 text-transparent bg-gradient-to-r from-white to-emerald-600 bg-clip-text'><p>Stay on top of your spending with real-time expense tracking and smart insights.
-          Visualize your financial habits and make informed decisions effortlessly.</p></p>
-      </div>
+
     </div>
   )
 }
 
 export default Workflow
+
