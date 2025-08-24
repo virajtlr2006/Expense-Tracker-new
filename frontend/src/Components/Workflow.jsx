4c5a4a8851f36a8ba7d071b86469cb13c7f6b4ea
Viraj Tailor <virajtlr2006@gmail.com>
2025-08-24 10:25:31 +0530

    Track expense button done

--- frontend/src/Components/Workflow.jsx
+++ frontend/src/Components/Workflow.jsx
@@ -1,13 +1,22 @@
-import { BadgePlus, File } from 'lucide-react'
+import { BadgePlus, CreditCardIcon, File, ListPlus } from 'lucide-react'
 import React from 'react'
 
 const Workflow = () => {
   return (
     <div className='  mt-20 '>
+
+      {/* Add Expense */}
       <div className='flex items-center'>
-        <BadgePlus className='text-green-800 ml-8 h-14 w-14'/>
-        <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ml-15'>Add Expense</button>
+        <ListPlus className='text-green-800 ml-8 h-14 w-14' />
+        <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ml-15' >Add Expense</button>
         <p className='text-xl ml-12 text-transparent bg-gradient-to-r from-white to-emerald-600 bg-clip-text'>Easily add your daily expenses and income with just a few clicks. Stay on top of your budget by tracking every transaction in real time.</p>
+      </div>
+      {/* Track expense */}
+      <div className='flex items-center mt-8'>
+        <CreditCardIcon className='text-green-800 ml-8 h-14 w-14' />
+        <button className='border-4 p-2 bg-gradient-to-r from-emerald-600 to-blue-950 rounded-2xl ml-17'>Track Expense</button>
+        <p className='text-xl ml-14 text-transparent bg-gradient-to-r from-white to-emerald-600 bg-clip-text'><p>Stay on top of your spending with real-time expense tracking and smart insights.
+          Visualize your financial habits and make informed decisions effortlessly.</p></p>
       </div>
     </div>
   )
